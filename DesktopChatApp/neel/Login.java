4eda7fccac010b045c82637fa84b2d413448c83b
Prazwal Thakur <89569590+prazwal-thakur@users.noreply.github.com>
2022-05-13 23:33:06 +0530

    Uploaded Files for the project

---
+++ DesktopChatApp/neel/Login.java
@@ -0,0 +1,74 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Login {
+public static void main(String [] args){
+	final JFrame login = new JFrame("Login");
+	JPanel panel = new JPanel();
+	final JTextField loginName = new JTextField(20);
+	JButton enter = new JButton("Login");
+	
+	panel .add(loginName);
+	panel.add(enter);
+	login.add(panel);
+	login.setSize(300, 100);
+	login.setVisible(true);
+	login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	enter.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			try {
+				ChatClient client = new ChatClient(loginName.getText());
+				login.setVisible(false);
+				login.dispose();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		}
+	});
+	
+	loginName.addKeyListener(new KeyListener() {
+		
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getKeyCode() == KeyEvent.VK_ENTER){
+				try {
+					ChatClient client = new ChatClient(loginName.getText());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				login.setVisible(false);
+				login.dispose();
+			}
+			
+		}
+	});
+}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Multi-Client-Chat-Application
+## A secure chat app for Personal Computers!!
+•	Used Java Sockets to communicates between machines <br> 
+•	Chat Server contains server socket and client socket so multiple clients can be communicating with a server on a given port. Each client connection is assigned a separate socket on that port.<br> 
+•	Use Vectors and StringTokenizer to create multiple users and precisely give feedback to all users about login/logout details and messages using Data input and output String from java.io and swing objects to create user Interface.
+
+### Screenshots
+![screenshot 906](https://user-images.githubusercontent.com/8587332/33235601-1244fc74-d1f0-11e7-9910-1da3f221f7a8.png)
